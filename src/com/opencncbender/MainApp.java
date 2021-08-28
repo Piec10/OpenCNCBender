6f6844b354243e725e4946669c00285a0a80e752
Piec10 <mateusz.piecka@gmail.com>
2021-08-28 13:28:19 +0200

    init

---
+++ src/com/opencncbender/MainApp.java
@@ -0,0 +1,14 @@
+package com.opencncbender;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+    public static void main(String[] args) { launch(args); }
+}
