98e13944590409e4691fae9de02e70d006909312
Piec10 <mateusz.piecka@gmail.com>
2021-11-12 16:29:36 +0100

    changed warning descriptions for settings files; non-modular for jar build;

--- src/main/java/com/opencncbender/MainApp.java
+++ src/main/java/com/opencncbender/MainApp.java
@@ -12,6 +12,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+
 import static com.opencncbender.util.PropertiesXMLWriter.storePropertiesToXML;
 
 public class MainApp extends Application {
